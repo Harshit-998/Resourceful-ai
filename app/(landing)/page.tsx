feed07df5e527c0a5f54f3d2a80e137766aada2e
Harshit-998 <harshityadav0767@gmail.com>
2023-08-16 19:10:26 +0530

    Landing page edited

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,15 +1,13 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
-import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
-  return ( 
+  return (
     <div className="h-full ">
       <LandingNavbar />
       <LandingHero />
-      <LandingContent />
     </div>
-   );
-}
- 
+  );
+};
+
 export default LandingPage;
